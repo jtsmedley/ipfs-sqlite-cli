68e387b22ad5ea3d711c42bc9661c02a5b4ef00b
jtsmedley <38006759+jtsmedley@users.noreply.github.com>
2021-12-09 21:46:08 -0600

    Update crypto.js
    Change default encryption to use GCM

--- crypto.js
+++ crypto.js
@@ -1,39 +1,53 @@
-const crypto = require('crypto');
-const _ = require('lodash');
+const crypto = require("crypto");
+const _ = require("lodash");
 
 class customCrypto {
-	#encryptionKey = undefined;
-	#encryptionIV = undefined;
+  #encryptionKey = undefined;
+  #encryptionIV = undefined;
 
-	constructor({algorithm, customKey, customIV}) {
-		this.#encryptionKey = customKey ? Buffer.from(customKey, 'hex') : crypto.randomBytes(32);
-		this.#encryptionIV = customIV ? Buffer.from(customIV, 'hex') : crypto.randomBytes(16);
+  constructor({ algorithm, customKey, customIV }) {
+    this.#encryptionKey = customKey
+      ? Buffer.from(customKey, "hex")
+      : crypto.randomBytes(32);
+    this.#encryptionIV = customIV
+      ? Buffer.from(customIV, "hex")
+      : crypto.randomBytes(16);
 
-		if (_.isString(customIV) === false || _.isString(customKey) === false) {
-			console.log(`Encryption Key: ${this.#encryptionKey.toString('hex')}`);
-			console.log(`Encryption IV: ${this.#encryptionIV.toString('hex')}`);
-		}
+    if (_.isString(customIV) === false || _.isString(customKey) === false) {
+      console.log(`Encryption Key: ${this.#encryptionKey.toString("hex")}`);
+      console.log(`Encryption IV: ${this.#encryptionIV.toString("hex")}`);
+    }
 
+    this.algorithm = algorithm || "aes-256-gcm";
+  }
 
-		this.algorithm = algorithm || 'aes-256-cbc';
-	}
+  encrypt(text) {
+    const cipher = crypto.createCipheriv(
+      this.algorithm,
+      this.#encryptionKey,
+      this.#encryptionIV
+    );
+    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
 
-	encrypt (text) {
-		const cipher = crypto.createCipheriv(this.algorithm, this.#encryptionKey, this.#encryptionIV);
-		const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+    return {
+      iv: this.#encryptionIV.toString("hex"),
+      content: encrypted,
+    };
+  }
 
-		return {
-			iv: this.#encryptionIV.toString('hex'),
-			content: encrypted
-		};
-	}
+  decrypt(hash) {
+    const decipher = crypto.createDecipheriv(
+      this.algorithm,
+      this.#encryptionKey,
+      Buffer.from(hash.iv || this.#encryptionIV, "hex")
+    );
+    const decrypted = Buffer.concat([
+      decipher.update(Buffer.from(hash.iv ? hash.content : hash, "hex")),
+      decipher.final(),
+    ]);
 
-	decrypt (hash) {
-		const decipher = crypto.createDecipheriv(this.algorithm, this.#encryptionKey, Buffer.from(hash.iv || this.#encryptionIV, 'hex'));
-		const decrypted = Buffer.concat([decipher.update(Buffer.from(hash.iv ? hash.content : hash, 'hex')), decipher.final()]);
-
-		return decrypted;
-	}
+    return decrypted;
+  }
 }
 
 module.exports = customCrypto;
